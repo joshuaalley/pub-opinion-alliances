53e11b3448dcaffa046ee57bda5f7b5242e2bea7
joshuaalley <you@example.com>
2021-09-06 15:55:05 -0400

    move all alliance char to appendix

--- data/present-results.R
+++ data/present-results.R
@@ -4,6 +4,34 @@
 # given number of attributes and no color, need more legible figures
 
 # choice for alliance maintenance
+main.choice.plot.el <- plot(filter.cregg.el(amce.main.choice)) + 
+  theme(legend.position = "none", axis.text.y = element_text(size = 10)) +
+  xlim(-.07, .13) +
+  scale_color_manual(values = rep("black", 10)) +
+  labs(title = "Maintain Alliance?")
+main.choice.plot.el
+
+
+# choice for alliance formation 
+form.choice.plot.el <- plot(filter.cregg.el(amce.form.choice)) + 
+  theme(legend.position = "none", axis.text.y = element_text(size = 10)) +
+  xlim(-.07, .13) +
+  scale_color_manual(values = rep("black", 10)) +
+  labs(title = "Form Alliance?")
+form.choice.plot.el
+
+
+
+# combine formation and maintenance choice plots
+grid.arrange(form.choice.plot.el, main.choice.plot.el, ncol= 2)
+joint.amce.plots.el <- arrangeGrob(form.choice.plot.el, main.choice.plot.el, ncol = 2)
+ggsave("figures/joint-amce-plots-el.png", joint.amce.plots.el, width = 10, height = 8)
+
+
+
+
+### Appendix figure w/ other char and elite cues
+# choice for alliance maintenance
 main.choice.plot <- plot(filter.cregg(amce.main.choice)) + 
   theme(legend.position = "none", axis.text.y = element_text(size = 10)) +
   xlim(-.07, .13) +
@@ -25,17 +53,7 @@ form.choice.plot
 # combine formation and maintenance choice plots
 grid.arrange(form.choice.plot, main.choice.plot, ncol= 2)
 joint.amce.plots <- arrangeGrob(form.choice.plot, main.choice.plot, ncol = 2)
-ggsave("figures/joint-amce-plots.png", joint.amce.plots, width = 10, height = 8)
-
-
-
-# partisanship
-mmplot.form.part <- plot(filter.cregg(mm.form.part), group = "party.id") +
-  facet_wrap(~BY, ncol = 3L) + 
-  theme_grey() +
-  theme(legend.position = "none", axis.text.y = element_text(size = 8)) +
-  ggtitle("Party ID and Alliance Formation")
-mmplot.form.part
+ggsave("appendix/joint-amce-plots.png", joint.amce.plots, width = 10, height = 8)
 
 
 
